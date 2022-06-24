9ff04c1ddce31a5523b5e3eb95989352a20054ae
sannansaleem <104469918+sannansaleem@users.noreply.github.com>
2022-06-23 21:00:05 -0400

    Update Employee_Database_Challenge.sql

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -55,3 +55,51 @@ WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no, ti.from_date DESC;
 
+-----------------------------------------------------------------------------------------------------------------------------
+--Deliverable 3: 
+------------------------------------------------------------------------------------------
+-- How many roles will need to be fill per title and department?
+SELECT DISTINCT ON (emp_no) e.emp_no, 
+			    e.first_name, 
+			    e.last_name, 
+			    e.birth_date, 
+			    de.from_date, 
+			    de.to_date, 
+			    t.title
+INTO employees_leaving
+FROM employees as e
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+	AND (de.from_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY e.emp_no;
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+							d.dept_name,
+							e.first_name, 
+							e.last_name, 
+							e.birth_date, 
+							de.from_date, 
+							de.to_date, 
+							t.title
+INTO employees_leaving_by_dept
+FROM employees as e
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+LEFT JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+	AND (de.from_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY e.emp_no;
+
+--group employees leavingby dept.
+SELECT COUNT(first_name) "Count", dept_name
+FROM employees_leaving_by_dept
+GROUP BY dept_name
+ORDER BY "Count" desc;
+
+
